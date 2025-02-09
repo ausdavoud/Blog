fc1930d623bb18bc2db5517b783723851d4e205a
Davoud Nosrati <davwvod@gmail.com>
2025-02-09 20:49:40 +0330

    Update config file

--- config.blog.js
+++ config.blog.js
@@ -1,29 +1,15 @@
-<<<<<<< HEAD:config.blog.ts
-import { Config } from "./app/utils/types"
-const config: Config = {
-    blog_name: 'TheWood',
-    description: 'Random Computer Science Stuff',
-    author: { name: 'Davooud Nosrati', url: 'https://github.com/thewoood' },
-    theme: 'auto',
-    navLinks: [
-        {
-            name: 'About',
-            href: '/pages/about'
-        }
-    ],
-=======
 const config = {
-    blog_name: 'Blogger',
-    description: 'Your favorite blog template',
+    blog_name: 'Ausdavoud',
+    description: 'Random Computer Science Stuff',
     theme: 'system',
     logo: "/logo.png",
     header: {
-        nav_links: [],
+        nav_links: [
+        ],
         blog_name: true,
         logo: false,
         theme_toggle: true,
     },
->>>>>>> 79d0aec227d88bb776716389c716e8274c09b5a4:config.blog.js
     lang: 'en',
     direction: 'ltr',
     content_entry: "./public",
@@ -33,8 +19,4 @@ const config = {
     metadata_base: process.env.DOMAIN || `https://${process.env.VERCEL_URL}` || `http://localhost:${process.env.PORT || 3000}`
 }
 
-<<<<<<< HEAD:config.blog.ts
-export default config
-=======
 module.exports =  config;
->>>>>>> 79d0aec227d88bb776716389c716e8274c09b5a4:config.blog.js
